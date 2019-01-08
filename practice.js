37d27b400c72280cf9d087b5ea6e02f18805a4a2
nateHamp <nathanhampton.pt@gmail.com>
2019-01-08 15:46:59 -0700

    required problems for 2nd afternoon assignment

--- practice.js
+++ practice.js
@@ -18,6 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
+function first(arr){
+  return arr[0];
+};
 
 
 
@@ -33,6 +36,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
+function last(arr){
+  return arr[2];
+};
 
 
 
@@ -48,6 +54,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
+function looper(arr){
+  for(var i = 0; i < arr.length; i++){
+    alert(arr[i]);
+  };
+};
 
 
 
@@ -63,6 +74,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+function reversedLooper(arr){
+  for(var i = arr.length-1; i>=0; i--){
+    alert(arr[i]);
+  };
+};
 
 
 
@@ -78,6 +94,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+function evenFinder(arr){
+  var newArray = [];
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i]%2 === 0){
+      newArray.push(arr[i])
+    };
+  };
+  return newArray;
+};
 
 
 
